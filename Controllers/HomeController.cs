7e40012fe23846b054232abb4846800b27e6e10e
Brandon Scott <brandon.e.scott@outlook.com>
2021-05-18 16:31:33 -0400

    Add files via upload
    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PiTempWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PiTempWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<PiTempData> entries = new List<PiTempData>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            con.ConnectionString = PiTempWebApp.Properties.Resources.ConnectionString;
+        }
+
+        public IActionResult Index()
+        {
+            FectchData();
+            return View(entries);
+        }
+
+        private void FectchData()
+        {
+            if(entries.Count > 0)
+            {
+                entries.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) [Temperature_F], [Temperature_C], [Humidity], [LogDate] FROM[dbo].[PiTempData] ORDER BY [LogDate] DESC";
+                dr = com.ExecuteReader();
+                while(dr.Read())
+                {
+                    entries.Add(new PiTempData()
+                    {
+                        Temperature_F = dr["Temperature_F"].ToString()
+                    ,
+                        Temperature_C = dr["Temperature_C"].ToString()
+                    ,
+                        Humidity = dr["Humidity"].ToString()
+                    ,
+                        LogDate = dr["LogDate"].ToString()
+                    });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PiTempData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiTempWebApp.Models
+{
+    public class PiTempData
+    {
+        public string Temperature_F { get; set; }
+        public string Temperature_C { get; set; }
+        public string Humidity { get; set; }
+        public string LogDate { get; set; }
+    }
+}
